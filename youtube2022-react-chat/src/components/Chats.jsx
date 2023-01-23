379025b728b7cbf92b6300db9a8bbaa036ae0184
ROHIT GUPTA <montygupta09091999@gmail.com>
2023-01-23 13:42:54 +0530

    mmm

--- youtube2022-react-chat/src/components/Chats.jsx
+++ youtube2022-react-chat/src/components/Chats.jsx
@@ -29,7 +29,7 @@ const Chats = () => {
   };
   return (
     <div className="chats">
-      {Object.entries(Chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
         <div
           className="userChat"
           key={chat[0]}
